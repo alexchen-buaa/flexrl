0eae38d09588a6334f1abd4303b400e0c85a6f95
alexchen-buaa <975106676@qq.com>
2022-11-21 20:41:48 +0800

    Add QR_DQN tests and fix gitignore

--- tests/test_atari.py
+++ tests/test_atari.py
@@ -1,12 +1,15 @@
-from flexrl.algorithms import PPO_Atari, DQN_Atari
+from flexrl.algorithms import PPO_Atari, DQN_Atari, QR_DQN_Atari
 
 args = {
     "exp_name": "test_atari",
     "gym_id": "PongNoFrameskip-v4",
-    "total_timesteps": 256,
+    "total_timesteps": 512,
+    "learning_starts": 256,
 }
 
 print("Testing PPO_Atari...")
 PPO_Atari(args)
 print("Testing DQN_Atari...")
 DQN_Atari(args)
+print("Testing QR_DQN_Atari...")
+QR_DQN_Atari(args)
--- tests/test_classic_control.py
+++ tests/test_classic_control.py
@@ -1,12 +1,15 @@
-from flexrl.algorithms import PPO, DQN
+from flexrl.algorithms import PPO, DQN, QR_DQN
 
 args = {
     "exp_name": "test_classic_control",
     "gym_id": "CartPole-v1",
-    "total_timesteps": 256,
+    "total_timesteps": 512,
+    "learning_starts": 256,
 }
 
 print("Testing PPO...")
 PPO(args)
 print("Testing DQN...")
 DQN(args)
+print("Testing QR_DQN...")
+QR_DQN(args)
